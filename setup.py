912c99166ee2ec35f2fef956e47233a40956ec08
dylanalexander <dylan@postriver.com>
2019-01-27 23:46:32 -0600

    Added docs and script infastrucute.

--- setup.py
+++ setup.py
@@ -7,5 +7,9 @@ setup(name='ficticious_corporeality',
         author='Dylan Alexander',
         author_email='dylan@postriver.com',
         license='GNU',
+        scripts=['bin/verse'],
+        install_requires=[
+            'matplotlib','numpy','scipy','pandas',
+            ],
         packages=['ficticious_corporeality'],
         zip_safe=False)
