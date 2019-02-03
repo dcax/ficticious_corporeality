565afca2cc6233c2febdeb9253b4de9a34c8e678
Aru Gyani <aru@gyani.com>
2019-02-02 19:30:58 -0600

    basic plotting of subsystems in place

--- ficticious_corporeality/plotting.py
+++ ficticious_corporeality/plotting.py
@@ -1,6 +1,22 @@
 # Plotting/GUI
-from pprint import pprint;
+from mpl_toolkits.mplot3d import Axes3D
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 def plot(manager):
+    #Plot setup
+    fig = plt.figure()
+    axes = fig.add_subplot(111, projection='3d')
+    xs = []
+    ys = []
+    #zs = [] Default is 0
 
-    print(manager.now.subsystems[0].get_particles()[0].loc[0])
+    #Iterates through every particle in the first subsystem.
+    #TODO: Expand to multi-colored and multi-subsystem plotting
+    for i in range(manager.now.subsystems[0].get_particles().size):
+        xs.append(manager.now.subsystems[0].get_particles()[i].loc[0])
+        ys.append(manager.now.subsystems[0].get_particles()[i].loc[1])
+    axes.scatter(xs, ys, zs=0, s = 20, c='red', depthshade = True)
+    plt.show()
+    print("test")
