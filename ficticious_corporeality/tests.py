1083d20a5905f2a0f2f9c113d5b2f3b86af3c885
dylanalexander <dylan@postriver.com>
2019-02-02 18:07:55 -0600

    Added new test case and option for nogui.

--- ficticious_corporeality/tests.py
+++ ficticious_corporeality/tests.py
@@ -3,9 +3,11 @@ from .constants import *
 from .subsystems import *
 from .plotting import *
 
-def test_suite():
+def test_suite(gui=True):
     verse_trial_1()
-    render()
+    if gui:
+        render()
+    verse_trial_2()
 
 #test
 def verse_trial_1():
@@ -23,3 +25,26 @@ def verse_trial_1():
     manager = VerseManager(initial_conditions=Instant.make_instant_from_subsystems([sheet1]),verse=v)
 
     manager.progress(n= 10, sample= lambda i, n: {i.report(n=n)}, every=2,ignoring_first=0)
+
+
+
+
+
+
+#tests
+
+def verse_trial_2(): #This test is designed to see how the parallelogram deals with chirality
+    square = ParallelogramBoundary(
+        np.array([10,10,0]),
+        np.array([10,0,0]),
+        np.array([0,0,0]),
+        uniform_margin=.1)
+
+    sheet1 = Sheet(name="major2", boundary=square, uniform_mass=1, n=5, m=5, constrained_movement=True, vertical_tension=10, horiz_tension=10)
+    sheet1.clump[1,1].v = np.array([0.0,0.0,10.0])
+
+    v = Verse1()
+
+    manager = VerseManager(initial_conditions=Instant.make_instant_from_subsystems([sheet1]),verse=v)
+
+    manager.progress(n= 10, sample= lambda i, n: {i.report(n=n)}, every=3,ignoring_first=0)
