a514a150cf2ec5361d304279db4f59309525c3cf
Aru Gyani <aru@gyani.com>
2019-02-02 20:16:57 -0600

    multi-subsystem plotting is now available
    note: multi-subsystem plotting, while available currently is not being displayed as manager.now.subsystems only is returning one subsystem

--- ficticious_corporeality/plotting.py
+++ ficticious_corporeality/plotting.py
@@ -2,21 +2,28 @@
 from mpl_toolkits.mplot3d import Axes3D
 
 import matplotlib.pyplot as plt
+from pprint import pprint
 import numpy as np
 
 def plot(manager):
     #Plot setup
     fig = plt.figure()
     axes = fig.add_subplot(111, projection='3d')
+    subsystems = manager.now.subsystems
+
+    #Values of x, y, and z for all particle values
     xs = []
     ys = []
-    #zs = [] Default is 0
+    zs = []
 
     #Iterates through every particle in the first subsystem.
-    #TODO: Expand to multi-colored and multi-subsystem plotting
-    for i in range(manager.now.subsystems[0].get_particles().size):
-        xs.append(manager.now.subsystems[0].get_particles()[i].loc[0])
-        ys.append(manager.now.subsystems[0].get_particles()[i].loc[1])
-    axes.scatter(xs, ys, zs=0, s = 20, c='red', depthshade = True)
+    #TODO: Expand to multi-colored and multi-subsystem plotting. (Multi-subsystem is available, however there is only one sub-system being received at the moment.)
+    for i in range(len(subsystems)):
+        for j in range(subsystems[i].get_particles().size):
+            xs.append(subsystems[i].get_particles()[j].loc[0])
+            ys.append(subsystems[i].get_particles()[j].loc[1])
+            zs.append(subsystems[i].get_particles()[j].loc[2])
+
+    #Scatter plot subsystem particles.
+    axes.scatter(xs, ys, zs, s = 20, c='red', depthshade = True)
     plt.show()
-    print("test")
